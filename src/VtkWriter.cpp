fecd45bded04195dbae3fdedda6614835a842b2b
Lukas Krenz <lukas@krenz.land>
2017-07-23 20:59:00 +0200

    Fix vtk writer.

--- src/VtkWriter.cpp
+++ src/VtkWriter.cpp
@@ -4,7 +4,7 @@ VtkWriter::VtkWriter(const std::string &filenameRoot, const coord_t &length)
     : filenameRoot(filenameRoot), length(length) {}
 
 void VtkWriter::write(const std::vector<double> &collideField, const std::vector<double> &mass,
-                      const std::vector<flag_t> &flagField, int t) {
+                      const std::vector<double> &density, const std::vector<flag_t> &flagField, int t) {
     std::stringstream filenameBuilder;
     // Filename is of the format name_i_j_k.t, where ijk are the mpi
     filenameBuilder << filenameRoot << "." << t << ".vtk";
@@ -21,28 +21,19 @@ void VtkWriter::write(const std::vector<double> &collideField, const std::vector
        << "ASCII \n\n"
        << "DATASET STRUCTURED_POINTS\n" // the grid type
        // Only the length without ghost cells is relevant here.
-       << "DIMENSIONS " << length[0] << ' ' << length[1] << ' ' << length[2] << '\n'
+       << "DIMENSIONS " << length[0] + 2 << ' ' << length[1] + 2 << ' ' << length[2] + 2 << '\n'
        // Each process has a different origin.
        << "ORIGIN 0 0 0" << '\n'
        << "SPACING 1 1 1 \n\n";
 
-    // We precompute the density for every cell.
-    const auto number_cells = (length[0] + 2) * (length[1] + 2) * (length[2] + 2);
-    const auto numberRealCells = length[0] * length[1] * length[2];
+    const auto noCells = (length[0] + 2) * (length[1] + 2) * (length[2] + 2);
 
-    auto density = std::vector<double>(number_cells);
-    for (size_t i = 0; i < density.size(); ++i) {
-        density[i] = computeDensity(&collideField[i * Q]);
-    }
-
-    os << "POINT_DATA " << numberRealCells << "\nVECTORS velocity float\n\n";
+    os << "POINT_DATA " << noCells << "\nVECTORS velocity float\n\n";
 
     double velocity[3] = {0};
     for (size_t i = 0; i < density.size(); ++i) {
-        if (flagField[i] != flag_t::PARALLEL_BOUNDARY) {
-            computeVelocity(&collideField[i * Q], density[i], velocity);
-            os << velocity[0] << ' ' << velocity[1] << ' ' << velocity[2] << '\n';
-        }
+        computeVelocity(&collideField[i * Q], density[i], velocity);
+        os << velocity[0] << ' ' << velocity[1] << ' ' << velocity[2] << '\n';
     }
 
     os << "\nSCALARS density float 1"
