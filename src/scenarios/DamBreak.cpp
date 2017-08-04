649a4cb8cd75ce7f6fa6d4f31553ed453faddf27
Andreas Reiser <andreasreiser93@gmail.com>
2017-08-04 11:00:52 +0200

    dam break

--- src/scenarios/DamBreak.cpp
+++ src/scenarios/DamBreak.cpp
@@ -12,4 +12,24 @@ void DamBreak::getFlagField(std::vector<flag_t> &flags, const coord_t &length) {
             }
         }
     }
+    for (int y = 1; y < length[1] + 1; ++y) {
+        for (int x = 1; x < length[0] + 1; ++x) {
+            flags[indexForCell(x, y, length[2], length)] = flag_t::EMPTY;
+        }
+    }
+
+//    for (int z = 1; z < length[1] + 1; ++z) {
+//        for (int x = 1; x < length[0] + 1; ++x) {
+//            flags[indexForCell(x, 1, z, length)] = flag_t::EMPTY;
+//            flags[indexForCell(x, length[1], z, length)] = flag_t::EMPTY;
+//        }
+//    }
+
+
+    for (int z = 1; z < length[2]/3; ++z) {
+        for (int y = 1; y < length[1]/2; ++y) {
+            flags[indexForCell(length[0]/2, y, z, length)] = flag_t::NO_SLIP;
+            flags[indexForCell(1+length[0]/2, y, z, length)] = flag_t::NO_SLIP;
+        }
+    }
 }
